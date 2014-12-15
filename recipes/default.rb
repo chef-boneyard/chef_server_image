beb8e55f1e3ccb84a6c664cdcf0c1d3176a6d4ee
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-12-15 17:22:10 +0530

    Fixed review comment to show warn if platform is other than ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,64 +16,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-chef_path = "/opt/chef/bin"
-temp_dir ||= Dir.mktmpdir
+case node['platform']
+when 'ubuntu'
+  chef_path = "/opt/chef/bin"
+  temp_dir ||= Dir.mktmpdir
 
-# install git
-apt_package "git" do
-end
+  # install git
+  apt_package "git" do
+  end
 
-# clone chef-repo
-git "#{ENV['HOME']}/chef-repo/" do
-  repository "#{node['chef-server-image']['chef-repo']}"
-  revision "master"
-end
+  # clone chef-repo
+  git "#{ENV['HOME']}/chef-repo/" do
+    repository "#{node['chef-server-image']['chef-repo']}"
+    revision "master"
+  end
 
-# download chef-server cookbook to chef-repo
-bash "chef-server cookbook download" do
-  cwd "#{temp_dir}"
-  code <<-EOH
-  sudo knife cookbook site download chef-server -f chef-server.tar.gz
-  tar -xzf #{temp_dir}/chef-server.tar.gz -C "#{ENV['HOME']}/chef-repo/cookbooks/"
-  EOH
-end
+  # download chef-server cookbook to chef-repo
+  bash "chef-server cookbook download" do
+    cwd "#{temp_dir}"
+    code <<-EOH
+    sudo knife cookbook site download chef-server -f chef-server.tar.gz
+    tar -xzf #{temp_dir}/chef-server.tar.gz -C "#{ENV['HOME']}/chef-repo/cookbooks/"
+    EOH
+  end
 
-# cleanup temp directory
-directory "#{temp_dir}" do
-  action :delete
-  recursive true
-end
+  # cleanup temp directory
+  directory "#{temp_dir}" do
+    action :delete
+    recursive true
+  end
 
-# place chef-server-setup.sh to chef package dir
-cookbook_file "chef-server-setup.sh" do
-  path "#{chef_path}/chef-server-setup"
-  mode "755"
-  action :create
-end
+  # place chef-server-setup.sh to chef package dir
+  cookbook_file "chef-server-setup.sh" do
+    path "#{chef_path}/chef-server-setup"
+    mode "755"
+    action :create
+  end
 
-# create symlink for /chef/opt/bin/chef-server-setup.sh to /usr/bin/
-link "#{chef_path}/chef-server-setup" do
-  to "/usr/bin/chef-server-setup"
-end
+  # create symlink for /chef/opt/bin/chef-server-setup.sh to /usr/bin/
+  link "#{chef_path}/chef-server-setup" do
+    to "/usr/bin/chef-server-setup"
+  end
 
-# creates attribute json file for chef-server cookbook
-template "/etc/chef/chef-server-attribute.json" do
-  source "chef-server-attribute.erb"
-  action :create
-end
+  # creates attribute json file for chef-server cookbook
+  template "/etc/chef/chef-server-attribute.json" do
+    source "chef-server-attribute.erb"
+    action :create
+  end
 
-cookbook_file "chef-server-setup-help.sh" do
-  path "#{chef_path}/chef-server-setup-help.sh"
-  mode "755"
-  action :create
-end
+  cookbook_file "chef-server-setup-help.sh" do
+    path "#{chef_path}/chef-server-setup-help.sh"
+    mode "755"
+    action :create
+  end
 
-# writes chef-server setup help message to .bashrc
-bash "update .bashrc to show chef-server configuration help" do
-  cwd "#{ENV['HOME']}"
-  code <<-EOH
-  cat '#{chef_path}/chef-server-setup-help.sh' >> .bashrc
-  EOH
+  # writes chef-server setup help message to .bashrc
+  bash "update .bashrc to show chef-server configuration help" do
+    cwd "#{ENV['HOME']}"
+    code <<-EOH
+    cat '#{chef_path}/chef-server-setup-help.sh' >> .bashrc
+    EOH
 
-  not_if { ::File.read("#{ENV['HOME']}/.bashrc").include?("chef-server-setup") } 
+    not_if { ::File.read("#{ENV['HOME']}/.bashrc").include?("chef-server-setup") } 
+  end
+else
+  Chef::Log.warn('chef-server-image recipes can only be run on the Ubuntu platform.')
 end
