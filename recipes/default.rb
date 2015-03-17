3b8de0b2f9861cf967b0bfd4ef6cb34429336661
prabhu-das <prabhu.das@clogeny.com>
2015-03-17 11:57:05 +0530

    Updated license agreement prompt to show license agreement URL and removed receipe for license file creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -59,16 +59,6 @@ when 'ubuntu'
     end 
   end
 
-  # creating LICENSE file on server on /opt/opscode location
-  license_file = File.expand_path('../../LICENSE', __FILE__)
-  file '/opt/opscode/LICENSE' do
-    content File.read("#{license_file}")
-    owner 'root'
-    group 'root'
-    mode '444'
-    action :create
-  end
-
   bash "update .bashrc to show chef-server configuration help" do
     cwd "#{ENV['HOME']}"
     code <<-EOH
